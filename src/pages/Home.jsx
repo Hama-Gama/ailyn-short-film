52a7bd05c0006d82c95f55bb66e15d49daa80403
Khamit <admiral3nelson7@gmail.com>
2025-10-06 22:08:12 +0500

    handle vercel error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import Footer from "../components/Footer";
-import Header from "../components/header";
+import Header from "../components/Header";
 import Hero from "../components/Hero";
 
 export default function Home() {
