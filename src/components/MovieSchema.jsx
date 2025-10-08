5f581d21461ada526a75212d61a9f76e9470008c
Khamit <admiral3nelson7@gmail.com>
2025-10-08 14:14:43 +0500

    open graph added

--- src/components/MovieSchema.jsx
+++ src/components/MovieSchema.jsx
@@ -3,7 +3,7 @@ import { Helmet } from 'react-helmet'
 import i18n from '../i18n' // импорт настройки i18next
 
 const MovieSchema = () => {
-	const lang = i18n.language // "ru", "en" или "kk"
+	const lang = i18n.language // "ru" | "en" | "kk"
 
 	// --- Мультиязычные мета-теги ---
 	const meta = {
@@ -27,83 +27,71 @@ const MovieSchema = () => {
 	const metaTitle = meta[lang]?.title || meta.en.title
 	const metaDescription = meta[lang]?.description || meta.en.description
 
-	// --- Данные Schema.org (Movie) ---
-	const movieData = {
-		ru: {
-			name: 'Айлин',
-			description:
-				'Официальный сайт фильма «Айлин». Узнайте о премьере, актёрах, сюжете и трейлере драмы 2026 года.',
-			url: 'https://ailynfilm.kz',
-			image: 'https://ailynfilm.kz/poster.jpg',
-			genre: 'Драма',
-			director: 'Unknown',
-			datePublished: '2025-12-15',
-			sameAs: ['https://www.imdb.com/title/tt1234567/'],
-		},
-		en: {
-			name: 'Ailyn',
-			description:
-				"Official website of the film 'Ailyn'. Learn about the 2026 premiere, cast, and trailer of this emotional drama.",
-			url: 'https://ailynfilm.kz',
-			image: 'https://ailynfilm.kz/poster.jpg',
-			genre: 'Drama',
-			director: 'Unknown',
-			datePublished: '2025-12-15',
-			sameAs: ['https://www.imdb.com/title/tt1234567/'],
-		},
-		kk: {
-			name: 'Айлин',
-			description:
-				'«Айлин» фильмінің ресми сайты. 2026 жылғы драманың премьерасы, актерлері мен трейлері туралы біліңіз.',
-			url: 'https://ailynfilm.kz',
-			image: 'https://ailynfilm.kz/poster.jpg',
-			genre: 'Драма',
-			director: 'Белгісіз',
-			datePublished: '2025-12-15',
-			sameAs: ['https://www.imdb.com/title/tt1234567/'],
-		},
+	// --- Данные о фильме ---
+	const data = {
+		name: 'Ailyn / Айлин',
+		url: 'https://ailynfilm.kz',
+		image: 'https://ailynfilm.kz/poster.jpg',
 	}
 
-	const data = movieData[lang] || movieData.en
-
+	// --- JSON-LD Schema.org (Movie) ---
 	const schema = {
 		'@context': 'https://schema.org',
 		'@type': 'Movie',
-		name: data.name,
+		name: metaTitle,
 		url: data.url,
 		image: data.image,
-		description: data.description,
-		genre: data.genre,
-		datePublished: data.datePublished,
+		description: metaDescription,
+		genre: 'Драма',
+		datePublished: '2025-12-15',
 		inLanguage: lang,
 		director: {
 			'@type': 'Person',
-			name: data.director,
+			name: 'Unknown',
 		},
 		publisher: {
 			'@type': 'Organization',
 			name: 'Ailyn Studio',
 			url: data.url,
 		},
-		sameAs: data.sameAs,
+		sameAs: ['https://www.imdb.com/title/tt1234567/'],
 	}
 
 	return (
 		<Helmet>
-			{/* Указываем язык сайта */}
+			{/* Язык страницы */}
 			<html lang={lang} />
 
-			{/* Title и meta description */}
+			{/* Title и метаописание */}
 			<title>{metaTitle}</title>
 			<meta name='description' content={metaDescription} />
 
-			{/* Альтернативные языковые версии */}
+			{/* --- Open Graph (для Facebook, Telegram, WhatsApp и др.) --- */}
+			<meta property='og:type' content='video.movie' />
+			<meta property='og:title' content={metaTitle} />
+			<meta property='og:description' content={metaDescription} />
+			<meta property='og:url' content={data.url} />
+			<meta property='og:image' content={data.image} />
+			<meta property='og:site_name' content='Ailyn Film' />
+			<meta
+				property='og:locale'
+				content={lang === 'kk' ? 'kk_KZ' : lang === 'ru' ? 'ru_RU' : 'en_US'}
+			/>
+
+			{/* --- Twitter Card --- */}
+			<meta name='twitter:card' content='summary_large_image' />
+			<meta name='twitter:title' content={metaTitle} />
+			<meta name='twitter:description' content={metaDescription} />
+			<meta name='twitter:image' content={data.image} />
+			<meta name='twitter:site' content='@AilynFilm' />
+
+			{/* --- Альтернативные языковые версии --- */}
 			<link rel='alternate' href='https://ailynfilm.kz/ru' hrefLang='ru' />
 			<link rel='alternate' href='https://ailynfilm.kz/en' hrefLang='en' />
 			<link rel='alternate' href='https://ailynfilm.kz/kk' hrefLang='kk' />
 			<link rel='alternate' href='https://ailynfilm.kz' hrefLang='x-default' />
 
-			{/* JSON-LD Schema.org */}
+			{/* --- Schema.org JSON-LD --- */}
 			<script type='application/ld+json'>{JSON.stringify(schema)}</script>
 		</Helmet>
 	)
